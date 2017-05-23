026b695915ecccd91af3656d62147b200be52bf9
gmajta <grzegorz.majta@krakweb.pl>
2017-05-23 09:00:28 +0200

    dodanie loggera

--- src/RagnarokExchangeSender/RagnarokExchangeSender.php
+++ src/RagnarokExchangeSender/RagnarokExchangeSender.php
@@ -15,6 +15,7 @@ use Krakweb\RagnarokProtocol\Interfaces\MSConnectorFactoryContract;
 use Krakweb\RagnarokProtocol\Interfaces\ServiceDiscoveryContract;
 use Krakweb\RagnarokProtocol\Microservice\MicroserviceInfo;
 use Krakweb\RagnarokProtocol\Protocol\RagnarokBaseExchangeMessage;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RagnarokExchangeSender implements ExchangeSenderContract
@@ -26,6 +27,9 @@ class RagnarokExchangeSender implements ExchangeSenderContract
 
     /** @var  MSConnectorFactoryContract */
     private $connectorFactory;
+    
+    /** @var  LoggerInterface */
+    private $logger;
 
 
     public function send(RagnarokBaseExchangeMessage $message)
@@ -92,6 +96,7 @@ class RagnarokExchangeSender implements ExchangeSenderContract
         $logContext = $message->getLogContext();
         if (!empty($logMessage)) {
             $connector->log($logMessage, $logContext);
+            $this->log($logMessage, $logContext);
         }
 
         $method = $message->getMethod();
@@ -142,6 +147,21 @@ class RagnarokExchangeSender implements ExchangeSenderContract
     {
         $this->connectorFactory = $connectorFactory;
     }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function log($message, array $context = [])
+    {
+        if (!$this->logger) {
+            return;
+        }
+
+        $this->logger->info($message, $context);
+    }
+
 
     private function generateRoutingKey(RagnarokBaseExchangeMessage $message)
     {
